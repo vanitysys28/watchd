87d5e250437101df27d2b5da4ea267d0b71e2f33
vanitysys28 <guiaubzer@gmail.com>
2024-06-14 23:28:42 +0200

    added function checking if video is playing

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function pausedChecker(){
 document.getElementsByTagName("video")[0].addEventListener('pause', () => {
         console.log('The video has been paused.');
+})
+}
+
+function playingChecker(){
+document.getElementsByTagName("video")[0].addEventListener('play', () => {
+        console.log('The video is playing.');
+})
 }
