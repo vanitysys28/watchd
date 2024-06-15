83d162948af4a958068f8f60176dbfa44a674424
vanitysys28 <guiaubzer@gmail.com>
2024-06-15 23:08:41 +0200

    added link between video state and video current time

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function pausedChecker(){
 document.getElementsByTagName("video")[0].addEventListener('pause', () => {
-        console.log('The video has been paused.');
+        console.log('The video has been paused at ' + getVideoCurrentTime());
 })
 }
 
 function playingChecker(){
 document.getElementsByTagName("video")[0].addEventListener('play', () => {
-        console.log('The video is playing.');
+        console.log('The video started playing at ' + getVideoCurrentTime());
 })
 }
 
 function getVideoCurrentTime(){
-   document.getElementsByTagName("video")[0].currentTime
+   return document.getElementsByTagName("video")[0].currentTime
 }
