6847a52adaf1a2e4264789044c9bef2e3378f516
vanitysys28 <guiaubzer@gmail.com>
2024-06-18 21:58:44 +0200

    modified event listeners functions to store video data if no duplicate data found

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ var videoData = []
 
 function pausedChecker(){
 document.getElementsByTagName("video")[0].addEventListener('pause', () => {
-        console.log('The video has been paused at ' + getVideoCurrentTime());
+    if (!checkDuplicateVideoData()){
+	storeVideoData()
+    };
 })
 }
 
 function playingChecker(){
 document.getElementsByTagName("video")[0].addEventListener('play', () => {
-        console.log('The video started playing at ' + getVideoCurrentTime());
+ if (!checkDuplicateVideoData()){
+	storeVideoData()
+    };
 })
 }
 
@@ -38,6 +42,7 @@ function checkDuplicateVideoData(){
     if (index !== -1) {
 	return true
     }
+    return false
 }
 
 function storeVideoData(){
