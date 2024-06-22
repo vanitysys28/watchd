4d27877f3a6cf2c28641041b31107593506d494f
vanitysys28 <guiaubzer@gmail.com>
2024-06-22 15:05:30 +0200

    beautified code

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
 var videoDataCollection = []
 
-function playingChecker(){
+function playingChecker() {
     document.querySelector("video").addEventListener('play', () => {
-	checkDuplicateVideoData()
-	storeVideoData()
-	backupVideoData(JSON.stringify(videoDataCollection))
-})
+        checkDuplicateVideoData()
+        storeVideoData()
+        backupVideoData(JSON.stringify(videoDataCollection))
+    })
 }
 
-function pausedChecker(){
+function pausedChecker() {
     document.querySelector("video").addEventListener('pause', () => {
-	checkDuplicateVideoData()
-	storeVideoData()
-	backupVideoData(JSON.stringify(videoDataCollection))
-})
+        checkDuplicateVideoData()
+        storeVideoData()
+        backupVideoData(JSON.stringify(videoDataCollection))
+    })
 }
 
-function getVideoID(){
-   return new URL(document.URL).searchParams.get('v')
+function getVideoID() {
+    return new URL(document.URL).searchParams.get('v')
 }
 
-function getVideoDuration(){
+function getVideoDuration() {
     return document.querySelector("video").duration
-}  
+}
 
-function getSegmentsPlayed(){
+function getSegmentsPlayed() {
     var segmentsPlayed = []
-    
-    for(var i = 0; i < document.querySelector("video").played.length; i++) { 
-	var intervalStart = document.querySelector("video").played.start(i)
-	var intervalEnd = document.querySelector("video").played.end(i)
-	segmentsPlayed.push(intervalStart + ":" + intervalEnd)
+
+    for (var i = 0; i < document.querySelector("video").played.length; i++) {
+        var intervalStart = document.querySelector("video").played.start(i)
+        var intervalEnd = document.querySelector("video").played.end(i)
+        segmentsPlayed.push(intervalStart + ":" + intervalEnd)
     }
-    
+
     return segmentsPlayed
 }
 
-function checkDuplicateVideoData(){
+function checkDuplicateVideoData() {
     var index = videoDataCollection.findIndex(video => video.id == getVideoID())
     if (index !== -1) {
-	videoDataCollection.splice(index, 1);
-    } 
+        videoDataCollection.splice(index, 1);
+    }
 }
 
-function calculateVideoPlaytimePercentage(segments){
+function calculateVideoPlaytimePercentage(segments) {
     var playtimeDuration = 0
-	    segments.forEach((segment) => playtimeDuration += segment.split(':')[1] - segment.split(':')[0]);
-	    return (playtimeDuration / getVideoDuration() * 100).toFixed(2)
+    segments.forEach((segment) => playtimeDuration += segment.split(':')[1] - segment.split(':')[0]);
+    return (playtimeDuration / getVideoDuration() * 100).toFixed(2)
 }
 
-function storeVideoData(){
-    var videoData = {id: getVideoID(), segments: getSegmentsPlayed()}
+function storeVideoData() {
+    var videoData = {
+        id: getVideoID(),
+        segments: getSegmentsPlayed()
+    }
     videoData.viewed = calculateVideoPlaytimePercentage(videoData.segments)
     videoDataCollection.push(videoData)
 }
 
-function backupVideoData(data){
-   localStorage.setItem('watchd', data);
+function backupVideoData(data) {
+    localStorage.setItem('watchd', data);
 }
 
-function fetchLocalStorage(){
+function fetchLocalStorage() {
     if (JSON.parse(localStorage.getItem("watchd"))) {
-videoDataCollection = JSON.parse(localStorage.getItem("watchd"))
+        videoDataCollection = JSON.parse(localStorage.getItem("watchd"))
     }
-   
+
 }
 
-function main(){
+function main() {
     fetchLocalStorage()
     playingChecker()
     pausedChecker()
